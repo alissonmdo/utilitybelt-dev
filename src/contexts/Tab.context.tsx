df1766ec4cb31a90828b103f0d12f934a509910f
Alisson Oliveira <me@alisson.dev>
2020-10-03 14:50:00 -0300

    ✨ Add: Context for current browser tab

---
+++ src/contexts/Tab.context.tsx
@@ -0,0 +1,32 @@
+import React, {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useState,
+} from "react";
+import { WithChildren } from "../types/WithChildren";
+
+export type TabContextType = {
+  tab?: chrome.tabs.Tab;
+  setTab?: Dispatch<SetStateAction<chrome.tabs.Tab | undefined>>;
+};
+export const TabContext = createContext<Partial<TabContextType>>({});
+
+type ProviderProps = WithChildren<unknown>;
+export function TabProvider({ children }: ProviderProps) {
+  const [tabState, setTabState] = useState<chrome.tabs.Tab>();
+
+  return (
+    <TabContext.Provider value={{ tab: tabState, setTab: setTabState }}>
+      {children}
+    </TabContext.Provider>
+  );
+}
+
+export function withTab<T>(Component: React.ComponentType<T & TabContextType>) {
+  return function WrapperComponent(props: T) {
+    const { tab, setTab } = useContext(TabContext);
+    return <Component {...props} tab={tab} setTab={setTab} />;
+  };
+}
